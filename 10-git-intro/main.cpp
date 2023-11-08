d86cda15ddc7de420d88d2289b0748358f014575
7aa7oo7aa7 <proniakin.aa@phystech.edu>
2023-11-08 11:00:25 +0300

    10 added

---
+++ 10-git-intro/main.cpp
@@ -0,0 +1,30 @@
+/*
+
+git
+
+windows: Git Bash
+linux: sudo apt-get install git
+macos: brew install git
+
+Вариант 1:
+git init - создать пустой репозиторий
+git remote add origin <url> - добавить удалённый репозиторий
+
+Вариант 2:
+git clone <url> - склонировать удалённый репозиторий
+
+git checkout -b NAME - создать ветку NAME (делайте это из main)
+git checkout NAME - переключиться на ветку NAME
+
+git add FILE - добавить файл FILE в очередь для коммита
+git add . - добавить всё в текущей директории
+
+git commit -m "your description" - сделать коммит в ветку
+
+git push origin NAME - добавить изменения в удалённый репозиторий в ветку NAME
+
+git status - вывести текущую ветку и список файлов для коммита
+git branch - вывести список веток и подсветить текущую ветку
+git log - вывести список коммитов
+
+*/
